4b86bded5fc3a60e45c4cbb1b110c9e9bc3ac252
Mehmet Kino <mehmetkino@yahoo.com>
2025-10-09 21:37:31 -0500

    added .lower() method/ random

---
+++ day4/randomazation.py
@@ -0,0 +1,13 @@
+import random
+#import my_module
+
+random_number = random.randint(1,10 )
+
+print(random_number) 
+# print(my_module.my_favorite_number)
+
+random_number_0_to_1 = random.random()
+print(random_number_0_to_1)
+
+number_float = random.uniform(1,10)
+print(number_float)
